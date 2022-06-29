333a54790419e67066522774f841c2c43cd52a61
mohamedabozeid2 <hamoashraf1960@gmail.com>
2022-06-29 23:58:19 +0200

    final commit

--- lib/modules/radio/radio.dart
+++ lib/modules/radio/radio.dart
@@ -7,11 +7,7 @@ class RadioPage extends StatelessWidget {
       children: const [
         Image(image: AssetImage('assets/images/quran_logo.png')),
         Divider(),
-        Text(
-          "اسمع على التلفزيون لحد ما اخلصه",
-          style: TextStyle(
-              color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
-        )
+
       ],
     );
   }
